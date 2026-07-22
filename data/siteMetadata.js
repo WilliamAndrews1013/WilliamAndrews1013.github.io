8cd065a937f06e5b4699a85365c6be95e9aecda4
William Andrews <williamandrews9292@outlook.com>
2026-07-22 16:09:37 -0400

    init

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,13 +1,13 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  title: 'Wags Dev',
+  author: 'William Andrews',
   headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://WilliamAndrews1013.github.io',
+  siteRepo: 'https://github.com/WilliamAndrews1013/WilliamAndrews1013.github.io',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
